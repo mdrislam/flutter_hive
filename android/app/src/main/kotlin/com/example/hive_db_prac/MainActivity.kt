64ba5231701c1603b2a4f881cc9424a8bed96274
Risad Hossain Home <mmdrislam@gmail.com>
2022-07-27 00:35:57 +0600

    init project flutter hive prac

---
+++ android/app/src/main/kotlin/com/example/hive_db_prac/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hive_db_prac
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/const.dart
@@ -0,0 +1,5 @@
+
+class MainUrl{
+final mainUrl ='sgd';
+
+}
